fcf112f4106bda17cd7f9b53749d1742892a401d
Disha Gupta <disha29082001@gmail.com>
2025-04-23 12:44:53 +0530

    done

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -8,6 +8,20 @@ router.post('/resolve', protect, authorizeRoles('admin'), handleDispute);
 
 // Admin rejecting a task
 router.put('/tasks/:id/reject', protect, authorizeRoles('admin'), rejectTask);
+router.get('/stats', protect, authorizeRoles('admin'), async (req, res) => {
+    const users = await User.countDocuments();
+    const tasks = await Task.countDocuments();
+    const revenue = await Submission.aggregate([
+      { $group: { _id: null, total: { $sum: "$paymentAmount" } } }
+    ]);
+  
+    res.json({
+      users,
+      tasks,
+      revenue: revenue[0]?.total || 0
+    });
+  });
+  
 
 
 module.exports = router;
